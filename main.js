76848a9b73a88bcb1118b17203108bc15086b106
randomsnakelol <82175745+randomsnakelol@users.noreply.github.com>
2021-05-15 08:41:54 -0700

    Update main.js

--- main.js
+++ main.js
@@ -1,51 +1,63 @@
 var canvas = document.getElementById("canvas");
 var ctx = canvas.getContext("2d");
-var hostingButton = document.getElementById("hosting");
+var hostingButton = document.getElementById("hostButton");
 var myConn = null;
 
 var text = 15;
 
+var chattingButton = document.getElementById("sendButton");
+chattingButton. disabled = true
+var chatting = function () {
+
+    myConn.send(document.getElementById('messages').value);
+    agnapfn
+    ctx.font = "20px Arial";
+    ctx.fillStyle = "#0000ff";
+    ctx.fillText(document.getElementById('messages').value, 5, text);
+    text += 20;
+
+};
+chattingButton.addEventListener("click", chatting);
+
+
 var isHosting = function () {
-  console.log(document.getElementById("channel").value, "is hosting");
-  var peer = new Peer(document.getElementById("channel").value);
-  peer.on("connection", function (conn) {
-    myConn = conn;
-    conn.on("data", function (data) {
-      ctx.font = "20px Arial";
-      ctx.fillStyle = "#808080";
-      ctx.fillText(data, 300, text);
-      text += 20;
+    console.log(document.getElementById('channel').value, "is hosting");
+    var peer = new Peer(document.getElementById('channel').value);
+    peer.on('connection', function (conn) {
+      chattingButton. disabled = false
+        myConn = conn;
+        conn.on('data', function (data) {
+          ctx.font = "20px Arial";
+          ctx.fillStyle = "#808080";
+          ctx.fillText(data, 300, text);
+          text += 20; 
+        });
     });
-  });
 };
 hostingButton.addEventListener("click", isHosting);
 
-var joiningButton = document.getElementById("ponnor7341");
-var isJoining = function () {
-  console.log(document.getElementById("channel").value, "is joining");
 
-  var peer = new Peer();
-  peer.on("open", function () {
-    var conn = peer.connect(document.getElementById("channel").value);
-    conn.on("open", function () {
-      myConn = conn;
-      myConn.on("data", function (data) {
-        ctx.font = "20px Arial";
-        ctx.fillStyle = "#808080";
-        ctx.fillText(data, 300, text);
-        text += 20;
-      });
+
+var joiningButton = document.getElementById("joinButton");
+var isJoining = function () {
+    console.log(document.getElementById('channel').value, "is joining");
+    var peer = new Peer();
+    peer.on("open", function () {
+        var conn = peer.connect(document.getElementById('channel').value); conn.on('open', function  () {
+          chattingButton. disabled = false
+            myConn = conn;
+            myConn.on('data', function (data) {
+              ctx.font = "20px Arial";
+              ctx.fillStyle = "#808080";
+              ctx.fillText(data, 300, text);
+              text += 20;
+            })
+        })
+        
     });
-  });
+
 };
+
 joiningButton.addEventListener("click", isJoining);
 
-var chattingButton = document.getElementById("ronnor7341");
-var chatting = function () {
-  myConn.send(document.getElementById("messages").value);
-  ctx.font = "20px Arial";
-  ctx.fillStyle = "#0000ff";
-  ctx.fillText(document.getElementById("messages").value, 5, text);
-  text += 20;
-};
-chattingButton.addEventListener("click", chatting); /////
+
